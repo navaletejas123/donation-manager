4e03b9fe43257b2efa2ca5fb42d6a5d0f24ec2e0
Tejas Navale <navaletejas1008@gmail.com>
2026-03-08 10:23:04 +0530

    add icons, build setting in package.json and much more

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const { app } = require('electron');
 
-const dbPath = path.join(__dirname, 'donation.db');
+// Use app.getPath('userData') to ensure the database is writable in production
+const dbPath = path.join(app.getPath('userData'), 'donation.db');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
-        console.log('Connected to the SQLite database.');
+        console.log('Connected to the SQLite database at:', dbPath);
         createTables();
     }
 });
